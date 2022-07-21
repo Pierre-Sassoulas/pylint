486f78c97981ad2c00520a7ab9c99950a294e088
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-07-21 17:41:05 +0200

    apply black on tests/functional/u/unnecessary/unnecessary_comprehension.py

--- tests/functional/u/unnecessary/unnecessary_comprehension.py
+++ tests/functional/u/unnecessary/unnecessary_comprehension.py
@@ -4,9 +4,9 @@
 # List comprehensions
 [x for x in iterable]  # [unnecessary-comprehension]
 [y for x in iterable]  # expression != target_list
-[x for x,y,z in iterable]  # expression != target_list
-[(x,y,z) for x,y,z in iterable]  # [unnecessary-comprehension]
-[(x,y,z) for (x,y,z) in iterable]  # [unnecessary-comprehension]
+[x for x, y, z in iterable]  # expression != target_list
+[(x, y, z) for x, y, z in iterable]  # [unnecessary-comprehension]
+[(x, y, z) for (x, y, z) in iterable]  # [unnecessary-comprehension]
 [x for x, *y in iterable]  # expression != target_list
 [x for x in iterable if condition]  # exclude comp_if
 [y for x in iterable for y in x]  # exclude nested comprehensions
@@ -19,11 +19,11 @@ item = [(k, v) for k, v in a_dict.items()]  # [unnecessary-comprehension]
 # Set comprehensions
 {x for x in iterable}  # [unnecessary-comprehension]
 {y for x in iterable}  # expression != target_list
-{x for x,y,z in iterable}  # expression != target_list
-{(x,y,z) for x,y,z in iterable}  # [unnecessary-comprehension]
-item = {(x,y,z) for (x, y, z) in iterable}  # [unnecessary-comprehension]
-{(x,y,z) for x in iterable}  # expression != target_list
-{(x,y,(a,b,c)) for x in iterable}  # expression != target_list
+{x for x, y, z in iterable}  # expression != target_list
+{(x, y, z) for x, y, z in iterable}  # [unnecessary-comprehension]
+item = {(x, y, z) for (x, y, z) in iterable}  # [unnecessary-comprehension]
+{(x, y, z) for x in iterable}  # expression != target_list
+{(x, y, (a, b, c)) for x in iterable}  # expression != target_list
 {x for x, *y in iterable}  # expression != target_list
 {x for x in iterable if condition}  # exclude comp_if
 {y for x in iterable for y in x}  # exclude nested comprehensions
@@ -32,7 +32,7 @@ item = {(x,y,z) for (x, y, z) in iterable}  # [unnecessary-comprehension]
 {x: y for x, y in iterable}  # [unnecessary-comprehension]
 {y: x for x, y in iterable}  # key value wrong order
 {x: y for (x, y) in iterable}  # [unnecessary-comprehension]
-{x: y for x,y,z in iterable}  # expression != target_list
+{x: y for x, y, z in iterable}  # expression != target_list
 {x: y for x, y in iterable if condition}  # exclude comp_if
 {y: z for x in iterable for y, z in x}  # exclude nested comprehensions
 {x: 1 for x in iterable}  # expression != target_list
@@ -44,6 +44,6 @@ my_dict = {}
 my_set = set()
 
 [elem for elem in my_list]  # [unnecessary-comprehension]
-items = {k: v for k, v in my_dict.items()} # [unnecessary-comprehension]
-{k: my_dict[k] for k in my_dict} # [consider-using-dict-items]
+items = {k: v for k, v in my_dict.items()}  # [unnecessary-comprehension]
+{k: my_dict[k] for k in my_dict}  # [consider-using-dict-items]
 {elem for elem in my_set}  # [unnecessary-comprehension]
