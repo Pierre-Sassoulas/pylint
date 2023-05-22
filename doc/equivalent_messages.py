eca4422369a25101b505d5041c79624f1e73bd02
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2023-05-22 22:27:32 +0200

    wip equivalent message

---
+++ doc/equivalent_messages.py
@@ -0,0 +1,15 @@
+# Licensed under the GPL: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+# For details: https://github.com/pylint-dev/pylint/blob/main/LICENSE
+# Copyright (c) https://github.com/pylint-dev/pylint/blob/main/CONTRIBUTORS.txt
+
+from __future__ import annotations
+
+from pathlib import Path
+
+DOC_MESSAGES_DIR = Path(__file__).parent / "data/messages"
+def get_all_bad_doc_examples() -> list[Path]:
+    for
+    return []
+
+if __name__ == "__main__":
+    print(get_all_bad_doc_examples())
